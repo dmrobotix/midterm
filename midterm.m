4d102d9da6d6618ad7bfa3b04f2b242dae5757c2
Margot Paez <mpz@ucsc.edu>
2016-02-11 19:16:44 -0800

    updating

--- midterm.m
+++ midterm.m
@@ -37,7 +37,31 @@ problemnull = null(B'*A'*A*B + B'*B);
 nullB = null(B);
 
 if nullB == problemnull
-    1
+    1;
 else
-    0
+    0;
 end
+
+% part h
+% a case when this is false
+A = [3 0 1; 1 0 3];
+problemh = A'*A;
+null(problemh')
+rank(problemh)
+size(problemh)
+null(A')
+rank(A)
+size(A)
+
+% part i
+% case when this is false
+
+u1 = [ 1 0 0]';
+u2 = [0 1 0]';
+u3 = [0 2 0]';
+u1'*u1
+u1'*u2
+u1'*u3
+u2'*u2
+u3'*u3
+u2'*u3
